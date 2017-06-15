f499a16c9b421bf343b9a340c4f9b9c164b8a746
Connor Hartley <vectrixu@gmail.com>
2017-06-15 18:37:58 +1200

    Move page data to parent

--- src/app.js
+++ src/app.js
@@ -28,44 +28,15 @@ var templateFooterPlate = fs.readFileSync(__dirname + '/templates/footer-plate.h
 // and states, each connected to a template.
 //
 // @author Connor Hartley
-// @version 0.0.3
+// @version 0.0.4
 
 // Navigation Plate
 Vue.component('navigation-plate', {
   template: templateNavigationPlate,
-  data: function() {
-    return {
-      pageIndex: 0,
-
-      pages: [
-        {
-          text: 'select',
-          displayText: 'Select',
-          isActive: true
-        },
-        {
-          text: 'timetable',
-          displayText: 'Timetable',
-          isActive: false
-        },
-        {
-          text: 'account',
-          displayText: 'Account',
-          isActive: false
-        },
-      ],
-    }
-  },
+  props: [ 'page-index', 'pages' ],
   methods: {
     changeTab: function (to) {
-      if (!this.pages[to].isActive) {
-        this.pages[this.pageIndex].isActive = false;
-
-        this.pages[to].isActive = true;
-        this.pageIndex = to;
-
-        this.$emit('updatePage')
-      }
+      this.$emit('update:pageIndex', to);
     }
   }
 });
@@ -78,5 +49,45 @@ Vue.component('footer-plate', {
 // VueJS Base
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: function () {
+    return {
+      pageIndex: 0,
+
+      pages: [
+        {
+          id: 'select',
+          displayId: 'Select',
+          isActive: true
+        },
+        {
+          id: 'timetable',
+          displayId: 'Timetable',
+          isActive: false
+        },
+        {
+          id: 'account',
+          displayId: 'Account',
+          isActive: false
+        },
+      ],
+    }
+  },
+  watch: {
+    // Watch for the page index to change.
+    pageIndex: function (newIndex, oldIndex) {
+      this.updatePage(newIndex, oldIndex);
+    }
+  },
+  methods: {
+    // Updates the content for children components due to a page change.
+    updatePage: function (to, from) {
+      if (!this.pages[to].isActive) {
+        this.pages[from].isActive = false;
+
+        this.pages[to].isActive = true;
+        this.pageIndex = to;
+      }
+    }
+  }
 });
